ab3bfb675f25a0502330b797abb76f76964dafdf
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-08-01 10:46:02 +0200

    #3 -> SpaceShip Movement & Turning

---
+++ SpaceShooter/Assets/Scripts/SpaceShipMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SpaceShipMovement : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 50)]
+    private float fSpeedOfShip = 20f;
+
+    [SerializeField]
+    [Range(1, 50)]
+    private float fTurnSpeedOfShip = 40f;
+
+    private float verticalAxisInput;
+    private float horizontalAxisInput;
+    private float pitchInput;
+    private float rollInput;
+    
+    private void Update()
+    {
+        //INPUTS
+
+        // MOVEMENT
+        verticalAxisInput = CrossPlatformInputManager.GetAxis("Vertical");
+        horizontalAxisInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        // TURN
+        pitchInput = CrossPlatformInputManager.GetAxis("Pitch");
+        // ROLL
+        rollInput = CrossPlatformInputManager.GetAxis("Roll");
+    }
+
+    private void FixedUpdate()
+    {
+        EngineThrust();
+        RotateShip();
+    }
+
+
+    private void EngineThrust()
+    {
+        if(verticalAxisInput > 0.1f)
+            transform.position += transform.forward * fSpeedOfShip * verticalAxisInput;
+    }
+
+    private void RotateShip()
+    {
+        float fRotShipY = fTurnSpeedOfShip * horizontalAxisInput * Time.deltaTime;
+        float fRotPitchX = fTurnSpeedOfShip * pitchInput * Time.deltaTime;
+        float fRotRollZ = fTurnSpeedOfShip * rollInput * Time.deltaTime;
+
+        transform.Rotate(-fRotPitchX, fRotShipY, fRotRollZ);
+    }
+}
