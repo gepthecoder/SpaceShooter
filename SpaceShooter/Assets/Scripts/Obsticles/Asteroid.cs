2730e77b41d0b89ef779ee364cdcfc8a8444db98
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-08-01 12:04:57 +0200

    #4 -> Asteroids prefabs, spawn manager

---
+++ SpaceShooter/Assets/Scripts/Obsticles/Asteroid.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    [Range(.3f, 25f)]
+    private float minScale = .6f;
+
+    [SerializeField]
+    [Range(1f, 50f)]
+    private float maxScale = 1.2f;
+
+    [SerializeField]
+    [Range(1f, 1000f)]
+    private float rotationSpeed = 100f;
+
+    private Vector3 asteroidScale;
+    private Vector3 asteroidRotation;
+
+    private void Start()
+    {
+        CreateAsteroid();
+    }
+
+    private void Update()
+    {
+        RotateAsteroid();
+    }
+
+    private void CreateAsteroid()
+    {
+        SetRandomLocalScale();
+        SetRandomRotation();
+    }
+
+    private void SetRandomLocalScale()
+    {//set random local scale of asteroid
+
+        asteroidScale = Vector3.one; // (1,1,1)
+        float scaleX = Random.Range(minScale, maxScale);
+        float scaleY = Random.Range(minScale, maxScale);
+        float scaleZ = Random.Range(minScale, maxScale);
+        asteroidScale.x = scaleX;
+        asteroidScale.y = scaleY;
+        asteroidScale.z = scaleZ;
+        transform.localScale = asteroidScale;
+    }
+
+    private void SetRandomRotation()
+    {//set random rotation of asteroid
+
+        float rotationX = Random.Range(-rotationSpeed, rotationSpeed);
+        float rotationY = Random.Range(-rotationSpeed, rotationSpeed);
+        float rotationZ = Random.Range(-rotationSpeed, rotationSpeed);
+
+        asteroidRotation.x = rotationX;
+        asteroidRotation.y = rotationY;
+        asteroidRotation.z = rotationZ;
+    }
+
+    private void RotateAsteroid()
+    {
+        transform.Rotate(asteroidRotation * Time.deltaTime);
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/Obsticles/AsteroidPlacer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidPlacer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] ASTEROIDS;
+
+    [SerializeField] [Range(10, 50)] private int numOfAsteroids = 10; //X^^3 asteroids
+    [SerializeField] [Range(1, 100)] private int spacingBetweenAsteroids = 5;
+
+
+    private void Start()
+    {
+        SpawnAsteroids();
+    }
+
+    private void SpawnAsteroids()
+    {
+        for (int X = 0; X < numOfAsteroids; X++)
+        {
+            for (int Y = 0; Y < numOfAsteroids; Y++)
+            {
+                for (int Z = 0; Z < numOfAsteroids; Z++)
+                {
+                    InstantiateAsteroid(X,Y,Z);
+                }
+            }
+        }
+    }
+
+    private void InstantiateAsteroid(int _x, int _y, int _z)
+    {
+        Vector3 pos = transform.position;
+        Vector3 offset = new Vector3(pos.x + (_x * spacingBetweenAsteroids) + AsteroidOffset(),
+                                        pos.y + (_y * spacingBetweenAsteroids) + AsteroidOffset(),
+                                            pos.z + (_z * spacingBetweenAsteroids) + AsteroidOffset());
+        Instantiate(RANDOM_ASTEROID(), offset, Quaternion.identity, transform); //clones are under parent
+    }
+
+    GameObject RANDOM_ASTEROID()
+    {
+        int r = Random.Range(0, ASTEROIDS.Length);
+        return ASTEROIDS[r];
+    }
+
+    float AsteroidOffset()
+    {
+        float r = Random.Range(-spacingBetweenAsteroids / 2f, spacingBetweenAsteroids / 2f);
+        return r;
+    }
+
+
+}
