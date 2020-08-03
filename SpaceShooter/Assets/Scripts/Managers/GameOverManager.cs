ae145dba1d49b1338322b0acb036c8a77113a3b5
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-08-03 20:19:04 +0200

    #17 -> GAME_OVER

---
+++ SpaceShooter/Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameOverManager instance;
+    public static GameOverManager Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType(typeof(GameOverManager)) as GameOverManager; }
+            return instance;
+        }
+        set { instance = value; }
+    }
+
+    void Awake() { instance = this; }
+    #endregion
+
+    [SerializeField] private Animator darkenScreenAnime;
+
+    public void GAME_OVER()
+    {
+        darkenScreenAnime.SetTrigger("darken");
+    }
+
+
+
+
+
+}
---
+++ SpaceShooter/Assets/Scripts/Managers/GameOverTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameOverTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator gameOverGuiAnime;
+
+    public void TRIGGER_GAME_OVER()
+    {
+        gameOverGuiAnime.SetTrigger("gameOver");
+    }
+}
