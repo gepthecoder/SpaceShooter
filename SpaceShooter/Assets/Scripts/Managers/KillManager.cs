98f521397b201485add3a2fc7cf8d2f378792cf0
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-08-03 20:23:41 +0200

    #18 -> destruction effect slider + HYPER JUMP; score manager

---
+++ SpaceShooter/Assets/Scripts/Managers/KillManager.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class KillManager : MonoBehaviour
+{
+    /*
+     Once you reach a certain amount of destruction rage you can perform hyper jump
+         */
+
+    #region Singleton
+    private static KillManager instance;
+    public static KillManager Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType(typeof(KillManager)) as KillManager; }
+            return instance;
+        }
+        set { instance = value; }
+    }
+
+    void Awake() { instance = this; }
+    #endregion
+
+    [SerializeField] private Animator[] popUpAnimators;
+    [SerializeField] private GameObject hyperJumpButtonObj;
+    [SerializeField] private Image destructionFillImage;
+    [SerializeField] private float fillSpeedInSec = .5f;
+    public int destructionCounter;
+
+    private int maxRage = 100;
+    private bool bCanPerformHyperJump;
+    public bool bActivateHyperJump;
+
+    private Button hyperButton;
+    private Animator fillImageAnime;
+
+    void Start()
+    {
+        destructionFillImage.fillAmount = .9f;
+        destructionCounter = 90;
+        bCanPerformHyperJump = false;
+
+        hyperButton = hyperJumpButtonObj.GetComponent<Button>();
+        fillImageAnime = destructionFillImage.GetComponent<Animator>();
+
+        hyperJumpButtonObj.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (bCanPerformHyperJump)
+        {
+            hyperButton.interactable = true;
+            hyperJumpButtonObj.SetActive(true);
+            fillImageAnime.SetBool("effect", true);
+            //activate slider effect
+            if (CrossPlatformInputManager.GetButtonDown("HyperJump"))
+            {
+                SlowTime();
+                bCanPerformHyperJump = false;
+                hyperButton.interactable = false;
+                //perform hyper jump
+                bActivateHyperJump = true;
+                StartCoroutine(HYPER_EFFECT_GUI());
+            }
+        }
+    }
+
+    private void SlowTime() { Time.timeScale = .5f; }
+    private void DefaultTime() { Time.timeScale = 1f; }
+
+    private IEnumerator HYPER_EFFECT_GUI()
+    {
+        while(destructionCounter > 0)
+        {
+            destructionCounter -= 1;
+            destructionFillImage.fillAmount = (float)destructionCounter / (float)maxRage;
+            yield return new WaitForSeconds(.01f);
+        }
+
+        if(destructionCounter <= 0)
+        {
+            bActivateHyperJump = false;
+            hyperJumpButtonObj.SetActive(false);
+            fillImageAnime.SetBool("effect", false);
+            DefaultTime();
+        }
+    }
+
+    public void IncreaseDestructionRage(int amount)
+    {
+        bool _filled = destructionFillImage.fillAmount >= 1;
+        if (_filled) { return; }
+
+        destructionCounter += amount;
+        Debug.Log("Destruction timer: " + destructionCounter);
+        StartCoroutine(ChangeToPct((float)destructionCounter / (float)maxRage));
+        PLAY_RANDOM_POPUP(amount);
+        if (destructionCounter >= maxRage)
+        {
+            bCanPerformHyperJump = true;
+        }
+    }
+
+
+    private IEnumerator ChangeToPct(float pct)
+    {
+        float preChangePct = destructionFillImage.fillAmount;
+        float elapsed = 0f;
+
+        while (elapsed < fillSpeedInSec)
+        {
+            elapsed += Time.deltaTime;
+            destructionFillImage.fillAmount = Mathf.Lerp(preChangePct, pct, elapsed / fillSpeedInSec);
+            yield return null;
+        }
+        destructionFillImage.fillAmount = pct;
+    }
+
+    private void PLAY_RANDOM_POPUP(int amount)
+    {
+        /*
+         Good=0, Great, Nice, Wow
+
+         */
+
+        bool better = amount > 10;
+        int randomTemp = Random.Range(0, 100);
+        if (better)
+        {
+            if(randomTemp <= 50)
+            {
+                popUpAnimators[1].SetTrigger("great");
+            }
+            else
+            {
+                popUpAnimators[3].SetTrigger("wow");
+            }
+        }
+        else
+        {
+            if (randomTemp <= 50)
+            {
+                popUpAnimators[0].SetTrigger("good");
+            }
+            else
+            {
+                popUpAnimators[2].SetTrigger("nice");
+            }
+        }
+    }
+
+}
---
+++ SpaceShooter/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    #region Singleton
+    private static ScoreManager instance;
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType(typeof(ScoreManager)) as ScoreManager; }
+            return instance;
+        }
+        set { instance = value; }
+    }
+    #endregion
+
+    public enum SCORE_EFFECTS { Yellow, Purple, Blue, Orange }
+
+    [SerializeField] private Text scoreText;
+
+    public Animator[] scorePopUps;
+    public int SCORE = 0;
+
+    void Awake()
+    {
+        instance = this;
+
+        if (PlayerPrefs.HasKey("SCORE"))
+        {
+            SCORE = PlayerPrefs.GetInt("SCORE", 0);
+        }
+        else
+        {
+            SAVE_SCORE();
+        }
+    }
+
+    void Start()
+    {
+        scoreText.text = "SCORE: " + SCORE;
+    }
+
+    public void SAVE_SCORE()
+    {
+        PlayerPrefs.SetInt("SCORE", SCORE);
+    }
+
+    public void UPDATE_SCORE(int byAmount)
+    {
+        StartCoroutine(scoreUpdater(byAmount));
+    }
+
+    private IEnumerator scoreUpdater(int byAmount)
+    {
+        int desiredScore = SCORE + byAmount;
+
+        while(SCORE != desiredScore)
+        {
+            if(SCORE < desiredScore)
+            {
+                SCORE += 5;
+                scoreText.text = "SCORE: " + SCORE;
+            }
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+
+    public void PLAY_RANDOM_POP_UP(string amount)
+    {
+        /*
+            Yellow = 0, Purple, Blue, Orange
+        */
+        int randomPopUp = Random.Range(0, scorePopUps.Length);
+        Text getPointsText = scorePopUps[randomPopUp].GetComponent<Text>();
+        if (getPointsText != null) { getPointsText.text = amount; }
+
+        switch (randomPopUp)
+        {
+            case (int)SCORE_EFFECTS.Yellow:
+                scorePopUps[randomPopUp].SetTrigger("Y");
+                break;
+            case (int)SCORE_EFFECTS.Purple:
+                scorePopUps[randomPopUp].SetTrigger("P");
+                break;
+            case (int)SCORE_EFFECTS.Blue:
+                scorePopUps[randomPopUp].SetTrigger("B");
+                break;
+            case (int)SCORE_EFFECTS.Orange:
+                scorePopUps[randomPopUp].SetTrigger("O");
+                break;
+            default:
+                scorePopUps[0].SetTrigger("Y");
+                break;
+        }   
+    }
+
+    private void OnDisable()
+    {
+        SAVE_SCORE();
+    }
+}
