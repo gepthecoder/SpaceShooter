4815231851070baf1e5f9ff3461ef4c0523ec87c
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-08-06 19:08:21 +0200

    #29 -> CollectableManager

---
+++ SpaceShooter/Assets/Scripts/Collectables/Collectable.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Collectable : MonoBehaviour
+{
+    public string Name;
+    // AUDIO CLIPS
+    public AudioClip pickUpSound;
+    //
+    private AudioSource aSource;
+
+    private float destroyTime;
+
+    bool rewardGiven;
+
+    void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.gameObject.tag == "Player")
+        {
+            if (!rewardGiven)
+            {
+                PLAY(pickUpSound);
+                COLLECT_REWARD(Name);
+            }
+            Destroy(gameObject, destroyTime);
+        }
+    }
+
+    void PLAY(AudioClip clip)
+    {
+        destroyTime = clip.length;
+        aSource.PlayOneShot(clip);
+    }
+
+    void COLLECT_REWARD(string rewardName)
+    {
+        switch (rewardName)
+        {
+            case "Diamond":
+                CurrencyManager.Instance.numOfDiamonds++;
+                CurrencyManager.Instance.SAVE_PREFS();
+
+                CurrencyManager.Instance.updateText_Diamonds();
+                break;
+            case "Crystal":
+                CurrencyManager.Instance.numOfCrystals++;
+                CurrencyManager.Instance.SAVE_PREFS();
+
+                CurrencyManager.Instance.updateText_Crystals();
+                break;
+            case "Fuel":
+                KillManager.Instance.destructionCounter += 20;
+                break;
+            case "Heart":
+                SpaceShipHealth.Instance.currentHealth += 20;
+                break;
+        }
+
+        rewardGiven = true;
+    }
+
+}
---
+++ SpaceShooter/Assets/Scripts/Collectables/CollectableManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableManager : MonoBehaviour
+{
+    #region Singleton
+    private static CollectableManager instance;
+    public static CollectableManager Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType(typeof(CollectableManager)) as CollectableManager; }
+            return instance;
+        }
+        set { instance = value; }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] private GameObject HEART;
+    [SerializeField] private GameObject DIAMOND;
+    [SerializeField] private GameObject CRYSTAL;
+    [SerializeField] private GameObject FUEL;
+
+    // SPAWN OBJECTS
+    public void SPAWN_FUEL(Transform spawnPos) { Instantiate(FUEL, spawnPos.position, Quaternion.identity); }
+
+    public void SPAWN_CRYSTAL(Transform spawnPos) { Instantiate(CRYSTAL, spawnPos.position, Quaternion.identity); }
+
+    public void SPAWN_DIAMOND(Transform spawnPos) { Instantiate(DIAMOND, spawnPos.position, Quaternion.identity); }
+
+    public void SPAWN_HEART(Transform spawnPos) { Instantiate(HEART, spawnPos.position, Quaternion.identity); }
+
+    public void SPAWN_RANDOM_COLLECTABLE(Transform spawnPos)
+    {
+        int random = Random.Range(0, 200);
+
+        if (random >= 0 && random < 50) { SPAWN_FUEL(spawnPos); }
+        else if (random >= 50 && random < 100) { SPAWN_DIAMOND(spawnPos); }
+        else if (random >= 100 && random < 150) { SPAWN_HEART(spawnPos); }
+        else if (random >= 150 && random < 200) { SPAWN_CRYSTAL(spawnPos); }
+    }
+}
