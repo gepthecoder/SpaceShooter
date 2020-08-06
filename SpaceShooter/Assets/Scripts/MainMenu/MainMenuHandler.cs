dc8b3d67bffd70f2501847cf9c97560f01cdee48
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-08-06 19:12:15 +0200

    #32 -> Added MainMenu

---
+++ SpaceShooter/Assets/Scripts/MainMenu/MainMenuHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public Button PlayButton;
+    public Image fillAmountImg;
+
+    float waitTime = 4f;
+
+    void Start()
+    {
+        fillAmountImg.fillAmount = 0f;
+    }
+
+    public void PLAY_GAME()
+    {
+        // START LOADING BAR
+        PlayButton.interactable = false;
+        StartCoroutine(StartLoading());
+    }
+
+    IEnumerator StartLoading()
+    {
+        fillAmountImg.fillAmount = 0f;
+
+        float timer = 0;
+
+        while (timer <= waitTime)
+        {
+            timer += Time.deltaTime;
+            fillAmountImg.fillAmount = timer / waitTime;
+            yield return null;
+        }
+
+        if (timer >= waitTime)
+        {
+            LoadGame();
+        }
+    }
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
